b1fcd32636d426c2681e45011c2c5def1c583e5b
nkosi-tauro <nkosilati23@yahoo.com>
2024-06-18 16:14:54 +0200

    added pagination requests

--- command_map.go
+++ command_map.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
 
 func callbackMap(cfg *config) error {
-
-	resp, err := cfg.pokeapiClient.ListLocationAreas()
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,5 +17,28 @@ func callbackMap(cfg *config) error {
 		fmt.Printf(" - %s\n", area.Name)
 	}
 
+	// // allow pagination
+	cfg.nextLocationURL = resp.Next
+	cfg.previousLocationURL = resp.Previous
+
 	return err
+}
+func callbackMapBack(cfg *config) error {
+	if cfg.previousLocationURL == nil {
+		return errors.New("no previous page")
+	}
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.previousLocationURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Location areas: ")
+	for _, area := range resp.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+
+	cfg.nextLocationURL = resp.Next
+	cfg.previousLocationURL = resp.Previous
+
+	return nil
 }
