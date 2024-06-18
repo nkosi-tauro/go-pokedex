f9db12f875e5baee6ea3f1f99f319d5250008250
nkosi-tauro <nkosilati23@yahoo.com>
2024-06-18 15:20:33 +0200

    added struct, function for creating and getting commands

--- repl.go
+++ repl.go
@@ -21,11 +21,40 @@ func startRepl() {
 		if len(cleaned) == 0 {
 			continue
 		}
+		//The first inputed word is the command
+		commandName := cleaned[0]
 
-
-		fmt.Println("echoing", cleaned)
+		availableCommands := getCommands()
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Invalid Command")
+			continue
+		}
+		command.callback()		
 	}
 
+}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays the available commands",
+			callback: callbackHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exits the program",
+			callback: callbackExit,
+		},
+	}
 }
 
 //Cleans up user input and returns each word as a Token
