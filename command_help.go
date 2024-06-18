1bc853efe802d0248d98679c9c94a4544fbc55bd
nkosi-tauro <nkosilati23@yahoo.com>
2024-06-18 15:19:50 +0200

    helper functions for each available command

---
+++ command_help.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Welcome to Nkosi's Pokedex.")
+	fmt.Println("Here are the available commands: ")
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println("")
+	return nil
+}
